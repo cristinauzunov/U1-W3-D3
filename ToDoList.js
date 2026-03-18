75b49c7bf2dd759cc31050f76f6dff8c47b61404
cristinauzunov <cristina.uzunov99@gmail.com>
2026-03-18 15:12:25 +0100

    ToDoList

---
+++ ToDoList.js
@@ -0,0 +1,35 @@
+
+const input = document.getElementById("new-task")
+const addButton = document.getElementById("add-button")
+const TaskList = document.getElementById("Task-list")
+
+function addTask(){
+    const text = input.value.trim()
+    if (!text) return
+
+const li = document.createElement("li")
+
+const label = document.createElement("span")
+label.textContent = text
+label.addEventListener("click", ()=>{
+    li.classList.toggle("done")
+})
+const deleteButton = document.createElement("button")
+deleteButton.textContent= "ELIMINA"
+deleteButton.addEventListener("click", ()=> li.remove())
+deleteButton.classList.add("deleteButton")
+
+li.appendChild(label)
+li.appendChild(deleteButton)
+TaskList.appendChild(li)
+
+input.value = ""
+input.focus()
+}
+
+
+addButton.addEventListener("click", addTask)
+
+
+
+
